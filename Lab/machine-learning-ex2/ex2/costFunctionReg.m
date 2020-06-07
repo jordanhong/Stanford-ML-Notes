836d8fbebac32a9a0456eb834054330a5915a4ed
Jordan Hong <jordanhong89@gmail.com>
2020-06-07 11:45:22 -0400

    Completed lab2

--- Lab/machine-learning-ex2/ex2/costFunctionReg.m
+++ Lab/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,21 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% Calculate hypothesis
+h = sigmoid(X*theta);
+
+% Calculate cost: the regular cost + summing square of theta-theta(1)^2
+reg = lambda/(2*m)*(sum(theta.^2)-theta(1)^2);
+J = 1/m* ( -y.'*log(h) - (1-y).'*log(1-h))+reg;
+
+% Calculate gradient
+grad = 1/m*X.'*(h-y)+lambda/m*theta;
+% modify grad(1)
+grad(1) = grad(1) - lambda/m*theta(1); 
+
+
+
+
 
 
 
